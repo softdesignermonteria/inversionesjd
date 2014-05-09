8fc39315f945d1e17d2d4b1279d1ebe65bc763d3
softdesignermonteria <administrador@softdesignermonteria.net>
2014-05-09 15:30:45 -0500

    cambios a recibos de caja

--- apps/default/controllers/recibos_caja_controller.php
+++ apps/default/controllers/recibos_caja_controller.php
@@ -224,6 +224,168 @@
 				echo json_encode($responce);
 		}
 		
+		
+		public function agregar_item_martesAction(){
+			
+				$this->setResponse("ajax");
+			
+				$db = DbBase::rawConnect();
+				$sql = "SELECT 
+						  creditos.empresa_id,
+						  creditos.clientes_id,
+						  creditos.fecha,
+						  detalle_cxc.id,
+						  detalle_cxc.idt,
+						  detalle_cxc.tipo_documento_id,
+						  detalle_cxc.prefijo,
+						  detalle_cxc.consecutivo,
+						  detalle_cxc.vencimiento,
+						  detalle_cxc.codigo,
+						  sum(detalle_cxc.valor * detalle_cxc.multiplicar) AS valor_cuota,
+						  ifnull((SELECT sum(rc.valor) AS valor FROM detalle_recibos_caja rc WHERE rc.detalle_cxc_id = detalle_cxc.id and rc.anulado = 0), 0) AS valor_pagado,
+						  ifnull((SELECT sum(nc.valor) AS valor FROM detalle_notas_credito nc WHERE nc.detalle_cxc_id = detalle_cxc.id and nc.anulado = 0), 0) AS valor_nota_credito
+						FROM
+						  creditos,
+						  detalle_cxc
+						  
+						WHERE
+						  
+						  creditos.id = detalle_cxc.creditos_id
+						  and detalle_cxc.anulado = 0
+						  and creditos.id = '".$_REQUEST["creditos_id"]."'
+						  and creditos.clientes_id = '".$_REQUEST["clientes_id"]."'
+						GROUP BY
+						  creditos.empresa_id,
+						  creditos.clientes_id,
+						  creditos.fecha,
+						  
+						  detalle_cxc.tipo_documento_id,
+						  detalle_cxc.prefijo,
+						  detalle_cxc.consecutivo,
+						  detalle_cxc.vencimiento,
+						  detalle_cxc.codigo,
+						  
+						  ifnull((SELECT sum(rc.valor) AS valor FROM detalle_recibos_caja rc WHERE rc.detalle_cxc_id = detalle_cxc.id and rc.anulado=0), 0),
+						  ifnull((SELECT sum(nc.valor) AS valor FROM detalle_notas_credito nc WHERE nc.detalle_cxc_id = detalle_cxc.id and nc.anulado = 0), 0)
+						HAVING
+						  (valor_cuota - valor_pagado - valor_nota_credito ) > 0
+						  and valor_cuota > 0 ";
+				$result = $db->query( $sql );
+				//echo $sql;
+				$valor_pagado = $_REQUEST["valor"];
+				$i=0;
+				while( $row = $db->fetchArray($result) ) {  
+					
+					$fecha = new Date($row[vencimiento]);
+					$dia_semana = $fecha->getDayNumberOfWeek();
+					//echo $dia_semana."<br />";
+					if($dia_semana==2){
+						
+						$cuota = $row[valor_cuota]-$row[valor_pagado]-$row[valor_nota_credito];
+						
+						if($valor_pagado >= $cuota ){ $valor = $cuota; }
+						if($valor_pagado < $cuota ){ $valor = $valor_pagado; }
+						if($valor>0){
+						$responce[$i] = array( 
+												"id" => "temp".rand(),
+												"detalle_cxc_id"    => $row[id],
+												"codigo"            => $row[codigo],
+												"numero_credito"    => $row[prefijo].$row[consecutivo],
+												"descripcion"       => "CANCELACION CUOTA CREDITO : ".$row[id]."-".$row[prefijo].$row[consecutivo]."-".$row[codigo],	
+												"valor"             => $valor,	
+												"vencimiento"       => $row[vencimiento],
+												"anulado"           => "NO"
+											);	
+						}
+							$valor_pagado = $valor_pagado - $valor;				
+					}
+					$i++;
+				}
+				echo json_encode($responce);
+		}
+		
+		
+		public function agregar_item_pagadoAction(){
+			
+				$this->setResponse("ajax");
+			
+				$db = DbBase::rawConnect();
+				$sql = "SELECT 
+						  creditos.empresa_id,
+						  creditos.clientes_id,
+						  creditos.fecha,
+						  detalle_cxc.id,
+						  detalle_cxc.idt,
+						  detalle_cxc.tipo_documento_id,
+						  detalle_cxc.prefijo,
+						  detalle_cxc.consecutivo,
+						  detalle_cxc.vencimiento,
+						  detalle_cxc.codigo,
+						  sum(detalle_cxc.valor * detalle_cxc.multiplicar) AS valor_cuota,
+						  ifnull((SELECT sum(rc.valor) AS valor FROM detalle_recibos_caja rc WHERE rc.detalle_cxc_id = detalle_cxc.id and rc.anulado = 0), 0) AS valor_pagado,
+						  ifnull((SELECT sum(nc.valor) AS valor FROM detalle_notas_credito nc WHERE nc.detalle_cxc_id = detalle_cxc.id and nc.anulado = 0), 0) AS valor_nota_credito
+						FROM
+						  creditos,
+						  detalle_cxc
+						  
+						WHERE
+						  
+						  creditos.id = detalle_cxc.creditos_id
+						  and detalle_cxc.anulado = 0
+						  and creditos.id = '".$_REQUEST["creditos_id"]."'
+						  and creditos.clientes_id = '".$_REQUEST["clientes_id"]."'
+						GROUP BY
+						  creditos.empresa_id,
+						  creditos.clientes_id,
+						  creditos.fecha,
+						  
+						  detalle_cxc.tipo_documento_id,
+						  detalle_cxc.prefijo,
+						  detalle_cxc.consecutivo,
+						  detalle_cxc.vencimiento,
+						  detalle_cxc.codigo,
+						  
+						  ifnull((SELECT sum(rc.valor) AS valor FROM detalle_recibos_caja rc WHERE rc.detalle_cxc_id = detalle_cxc.id and rc.anulado=0), 0),
+						  ifnull((SELECT sum(nc.valor) AS valor FROM detalle_notas_credito nc WHERE nc.detalle_cxc_id = detalle_cxc.id and nc.anulado = 0), 0)
+						HAVING
+						  (valor_cuota - valor_pagado - valor_nota_credito ) > 0
+						  and valor_cuota > 0 ";
+				$result = $db->query( $sql );
+				//echo $sql;
+				$valor_pagado = $_REQUEST["valor"];
+				$i=0;
+				while( $row = $db->fetchArray($result) ) {  
+					
+					$fecha = new Date($row[vencimiento]);
+					$dia_semana = $fecha->getDayNumberOfWeek();
+					//echo $dia_semana."<br />";
+					//if($dia_semana==2){
+						
+						$cuota = $row[valor_cuota]-$row[valor_pagado]-$row[valor_nota_credito];
+						
+						if($valor_pagado >= $cuota ){ $valor = $cuota; }
+						if($valor_pagado < $cuota ){ $valor = $valor_pagado; }
+						if($valor>0){
+						$responce[$i] = array( 
+												"id" => "temp".rand(),
+												"detalle_cxc_id"    => $row[id],
+												"codigo"            => $row[codigo],
+												"numero_credito"    => $row[prefijo].$row[consecutivo],
+												"descripcion"       => "CANCELACION CUOTA CREDITO : ".$row[id]."-".$row[prefijo].$row[consecutivo]."-".$row[codigo],	
+												"valor"             => $valor,	
+												"vencimiento"       => $row[vencimiento],
+												"anulado"           => "NO"
+											);	
+						}
+							$valor_pagado = $valor_pagado - $valor;				
+					//}
+					$i++;
+				}
+				echo json_encode($responce);
+		}
+		
+		
+		
 		public function agregar_interesesAction(){
 
 			$this->setResponse("ajax");
@@ -242,289 +404,8 @@
 			
 		}
 		
-		/**public function agregar_interesesAction(){
-
-			$this->setResponse("ajax");
-			$db2 = DbBase::rawConnect();
-			
-			$creditos_id = $_REQUEST["creditos_id"];
-			$fecha_recibo       = $_REQUEST["fecha"];
-			$clientes_id = $_REQUEST["clientes_id"];
-			//CONSULTA ULTIMO DIA EN QUE PAGO INTERESES.
-			
-			$creditos = $this->Creditos->findFirst(" id = '$creditos_id' ");
-			$fecha = $creditos->fecha;
-			
-			$rec = new RecibosCaja();
-			$sql = "select r.* 
-						from 
-						recibos_caja r, detalle_recibos_caja dr
-						where r.id = dr.recibos_caja_id
-						and dr.anulado = '0'
-						and dr.descripcion like 'INTERESES%'
-						and r.clientes_id = '$clientes_id'
-						and r.creditos_id = '$creditos_id'
-						
-						order by r.fecha desc
-						limit 1
-						";
-			if( $rec->countBySql($sql) > 0 ){
-				$rec2 = $db2->inQueryAssoc($sql);
-				foreach($rec2 as $rec2):
-					Flash::notice($rec2[prefijo].$rec2[consecutivo]." Ultimo Recibo caja con intereses fue pagado el dia ".$rec2[fecha] );
-					$fecha = $rec2[fecha];
-				endforeach;
-			}else{
-				Flash::notice("Recibos de caja con intereses no encontrados");
-			}
-			
-			$abonos_mes = 0;
-			$saldo_deuda = 0;
-			$saldo_deuda_sin_prestamos = 0;
-			$prestamos_mes = 0;
-			$intereses_mes = 0;
-			$intereses_prestamos = 0;
-			
-			
-			
-			$notas = new DetalleNotasDebito();
-			$sql_notas = "select sum(dnd.valor) as valor, 
-								 nd.fecha, 
-								 (datediff('$fecha_recibo', nd.fecha ) + 1) as dias, 
-								 nd.porcentaje,
-								 round( ( ( sum(dnd.valor) * ( nd.porcentaje /100 ) ) /30 ) * (datediff('$fecha_recibo', nd.fecha ) + 1) ,-3)as intereses
-							from 
-								notas_debito nd,
-								detalle_notas_debito dnd,
-								creditos cr
-							where
-								nd.id = dnd.notas_debito_id
-								and dnd.anulado = 0
-								and nd.creditos_id = '$creditos_id' 
-								and nd.clientes_id = '$clientes_id' 
-								and nd.creditos_id = cr.id 
-								and concat(year(nd.fecha),month(nd.fecha)) = concat(year('$fecha_recibo'),month('$fecha_recibo')) 
-								group by nd.id
-							";
-					
-				
-				$notas = $db2->inQueryAssoc($sql_notas);
-				$i=0;
-				foreach($notas as $filas) {
-					$i++;
-					Flash::notice("$i - Prestamos Mes por Valor: ".$filas[valor]." Dias: ". $filas[dias] ." Intereses: ".$filas[intereses]);
-					 //Flash::notice($filas[valor]);
-					 //Flash::notice($filas[fecha]);
-  				     //Flash::notice($filas[dias]);
-					 //Flash::notice($filas[porcentaje]);
-					 //Flash::notice($filas[intereses]);
-					 $prestamos_mes += $filas[valor];
-					 $intereses_prestamos += $filas[intereses];
-				}
-				
-			$cxc = new Cxc();
-			$cxc = $cxc->findFirst(" creditos_id = '$creditos_id' and anulado=0 ");
-			
-			$saldo_deuda = $cxc->valor_total + $cxc->pagado;
-			Flash::notice("Saldo Deduda: ".$saldo_deuda);
-			$saldo_deuda_sin_prestamos = $cxc->valor_total + $cxc->pagado - $prestamos_mes;
-			Flash::notice("Saldo Deduda Menos Prestamos Mes: ".$saldo_deuda_sin_prestamos);
-			
-			$filas = $db2->fetchOne("select round( ( ( $saldo_deuda_sin_prestamos * ( $creditos->porcentaje /100 ) ) /30 ) * (datediff('$fecha_recibo','$fecha') + 1) ,-3)as intereses ");
-			Flash::notice($filas[intereses]);
-			//$fecha_temp = new Date("2008-01-02");
-			//echo $fechatemp->diffDate("2008-01-10"); //Devuelve -
-			
-			$intereses_mes = $filas[intereses];
-			
-			$valor_int = $intereses_mes + $intereses_prestamos;
-			$sql = "select r.* 
-						from 
-						recibos_caja r, detalle_recibos_caja dr
-						where r.id = dr.recibos_caja_id
-						and dr.anulado = '0'
-						and dr.descripcion like 'INTERESES%'
-						and r.clientes_id = '$clientes_id'
-						and r.creditos_id = '$creditos_id'
-						and concat(year(r.fecha),month(r.fecha)) = concat(year('$fecha_recibo'),month('$fecha_recibo')) 
-						order by r.fecha desc
-						";
-			if( $rec->countBySql($sql) > 0 ){
-				$valor_int = 0;
-				Flash::notice("Ya fueron cobrados intereses en este mes.");
-			}
-			
-			//$this->setParamToView("valor_int",$valor_int);
-			$this->setParamToView("valor_int",$valor_int);
-			$this->setParamToView("fecha_recibo",$fecha_recibo);
-			
-			$db2->close();
-		}
-		
-		public function agregar_intereses_itemAction(){
-			$this->setResponse("ajax");
-			
-		}**/
-		
-/*
-		public function addAction(){
-
-			$this->setResponse('view');
-			/*echo "<script>jQuery(\"#guardar\").Attr(\"disabled\",\"disabled\");</script>";*/
-		/*	$deshabilitar = "<script>jQuery(\"#guardar\").removeAttr(\"disabled\");</script>";
-			$sw=0;
-			$msg_error="";
-			$total_credito=0;
-			
-			if($sw==0){
-					//abriando transacciones
-				Flash::success("EMPEZANDO A GUARDAR LOS REGISTOS...");	
-				$transaction = new ActiveRecordTransaction(true);   
-				try{
-					$transaction = TransactionManager::getUserTransaction(); 
-					$this->Consecutivos->setTransaction($transaction); 
-					$this->DetalleConsecutivos->setTransaction($transaction); 
-					$prefijo = '';  	$consecutivo = ''; 	$tipo_documento_id = '';
-
-							if( $_REQUEST["id"] == ''){
-								$cons = $this->DetalleConsecutivos->findFirst("activo = '0' and tipo_documento_id = '".$_REQUEST['tipo_documento_id']."'");
-								$id = $cons->id;
-								$prefijo = $cons->prefijo;
-								$consecutivo = $cons->desde;
-								$tipo_documento_id = $cons->tipo_documento_id;
-								//Flash::error("$id");
-								//Flash::error("$prefijo");
-								//Flash::error("$consecutivo");
-								//Flash::error("$tipo_documento_id");
-								//Flash::error($_REQUEST['tipo_documento_id']);
-								$dtc = new DetalleConsecutivos();
-								$dtc = $this->DetalleConsecutivos->findFirst("id = '$id'");
-								$dtc->setTransaction($transaction);
-								$dtc->desde = $dtc->desde+1;
-									if($dtc->save()==false){
-											echo $deshabilitar;
-											
-											foreach($dtc->getMessages() as $message){ 
-												$msg_error.=Flash::error("TABLA DETALLE CONSECUTIVOS: ".$message);  
-												Flash::error("TABLA DETALLE CONSECUTIVOS: ".$message); 
-										}
-										$transaction->rollback();
-									}
-									
-							}else{
-								$prefijo = $_REQUEST["prefijo"];
-								$consecutivo = $_REQUEST["consecutivo"];
-								$tipo_documento_id = $_REQUEST["tipo_documento_id"];
-							}
-				
-				
-							//Flash::success("VERIFICANDO LOS CONSECUTIVOS PARA ESTE DOCUMENTO");		
-							 $encabezado = new RecibosCaja();
-							 $encabezado->setTransaction($transaction);
-							//para traer el mismo modelo ya instanciado
-							 $encabezado->id                  =  $_REQUEST["id"];
-							 $encabezado->empresa_id          = $_REQUEST["empresa_id"];
-							 $encabezado->clientes_id         = $_REQUEST["clientes_id"];
-							 $encabezado->creditos_id         = $_REQUEST["creditos_id"];
-							 $encabezado->tipo_documento_id   = $cons->tipo_documento_id;
-							 $encabezado->prefijo             = $cons->prefijo;
-							 $encabezado->consecutivo         = $cons->desde;   
-							 $encabezado->fecha_act           = date("Y-m-d H:i:s");
-							 $encabezado->hora_act            = date("H:i:s");
-							 $encabezado->fecha               = $_REQUEST["fecha"];
-							 $encabezado->anulado             = '0';
-							 $encabezado->activo              = '0';
-							 $encabezado->observaciones       = $_REQUEST["observaciones"];
-							 $encabezado->cheque              = $_REQUEST["cheque"];
-							 $encabezado->banco               = $_REQUEST["banco"];
-						
-							 //Flash::error($cons->desde);
-								if($encabezado->save()==false){
-									//abre for each
-									foreach($encabezado->getMessages() as $message){ 
-										Flash::error("TABLA Recibos Caja: ".$message); 
-										$msg_error.=Flash::error("TABLA Recibos Caja: ".$message); 
-									}
-									//cierra foreach
-									$transaction->rollback();
-								//cierra if
-								}
-							
-							$total_credito = 0;
-							$detalles_item = str_replace("]\"","]",str_replace("\"[","[",str_replace("\\","",$_POST["detalles"])));
-							//Flash::notice($detalles_item);
-							if($detalles_item!='[]'){	
-								
-								if(json_decode($detalles_item)){
-									Flash::success("Json Correcto");
-									$detalles_item = json_decode($detalles_item);
-								}else{
-									echo $deshabilitar;
-									Flash::error("Error json");
-									$msg_error.=Flash::error("Error json"); 
-									$transaction->rollback();
-									
-								}
-	
-								
-								foreach( $detalles_item as $items):
-									$detalles = new DetalleRecibosCaja();
-									$detalles->setTransaction($transaction);
-									//$detalles->id                     = $items->id;
-									if( trim(substr($items->id,0,4)) == trim('temp') ){
-										$detalles->id                     = '0';
-									}else{
-										$detalles->id                     = $items->id;
-									}
-									$detalles->detalle_cxc_id         = $items->detalle_cxc_id;
-									$detalles->recibos_caja_id        = $encabezado->id;
-									$detalles->descripcion            = $items->descripcion;
-									$detalles->valor                  = $items->valor;
-									$detalles->codigo                 = $items->codigo;
-									$detalles->vencimiento            = $items->vencimiento;
-									$detalles->descuento              = 0;
-									//$detalles->anulado                = '0';
-									if($items->anulado == "SI") {  $detalles->anulado = '1';}
-									if($items->anulado == "NO") {  $detalles->anulado = '0';  $total_credito += $detalles->valor; }
-									
-										if($detalles->save()==false){
-											echo $deshabilitar;
-											foreach($detalles->getMessages() as $message){ 
-												$msg_error.=Flash::error(" TABLA DETALLE RECIBOS DE CAJA : ".$message); 
-												Flash::error(" TABLA DETALLE RECIBOS DE CAJA: ".$message); 
-											}
-											$transaction->rollback();
-											
-										}	
-										
-													  
-								endforeach; //cierra los detalles		
-							
-						} //cierra detalles json	
-
-											
-
-					$transaction->commit();
-	
-					Flash::success("RECIBO DE CAJA GUARDADA SATISFACTORIAMENTE");	
-					echo "<script>quitar_mensajes();</script>";
-					echo "<script>redireccionar_action('".Router::getController()."/show/$encabezado->id');</script>";
-				}catch(TransactionFailed $e){		
-					Flash::error($e->getMessage());
-					$msg_error.=Flash::error($e->getMessage()); 
-						echo "<script>";
-						echo "jQuery('dialogo_mensajes').html('');";	
-						echo "jQuery('dialogo_mensajes').html('$msg_error');";	
-						echo "jQuery('dialogo_mensajes').dialog('open');";	
-						echo "</script>";
-					//cierre cacth try
-				}
-		  }//cierra if todo bien
-		  
-		  
 		
 
-}*/
 
 
 		public function addAction(){
@@ -655,6 +536,11 @@
 								}
 								
 								
+								$creditos_tmp  = $this->Creditos->findFirst("id = '$encabezado->creditos_id' and anulado = 0 ");
+								$porcentaje    = $creditos_tmp->porcentaje/100; 
+							
+								Flash::notice(" porcentaje $porcentaje "); 
+								
 								//$saldotmp = $saldo;
 								foreach( $detalles_item as $items):
 									$capitaltmp = 0;
@@ -669,20 +555,7 @@
 										$detalles->id                     = $items->id;
 									}
 									
-									$dcxc = new DetalleCxc();
-								    $dcxc->setTransaction($transaction);
-								    $dcxc = $this->DetalleCxc->findFirst("id = '$items->detalle_cxc_id' and anulado = 0");
 									
-									/*si es credito*/
-									if($dcxc->tipo_documento_id == $creditostipo){
-										$creditos_tmp = $this->Creditos->findFirst("id = '$encabezado->creditos_id' and anulado = 0 ");
-										$porcentaje    = $creditos_tmp->porcentaje/100;
-									}
-									/*si es notas debito*/
-									if($dcxc->tipo_documento_id == $notastipo){
-										$notasd_tmp = $this->NotasDebito->findFirst("id = '$encabezado->creditos_id' and anulado = 0 ");
-										$porcentaje = $notasd_tmp->porcentaje/100;
-									}
 								    
 									$capitaltmp = $items->valor / (1 + $porcentaje );
 									
@@ -899,6 +772,11 @@
 								}
 								
 								
+								$creditos_tmp  = $this->Creditos->findFirst("id = '$encabezado->creditos_id' and anulado = 0 ");
+								$porcentaje    = $creditos_tmp->porcentaje/100; 
+							
+								Flash::notice(" porcentaje $porcentaje "); 
+							
 								$saldotmp = $saldo;
 								foreach( $detalles_item as $items):
 									
@@ -915,31 +793,10 @@
 										$detalles->id                     = $items->id;
 									}
 									
-									if($detalles->anulado == 0){
 									
-											
 									
-											$dcxc = new DetalleCxc();
-											$dcxc->setTransaction($transaction);
-											$dcxc = $this->DetalleCxc->findFirst("id = '$items->detalle_cxc_id' and anulado = 0");
 									
-											/*si es credito*/
-											if($dcxc->tipo_documento_id == $creditostipo){
-												$creditos_tmp = $this->Creditos->findFirst("id = '$encabezado->creditos_id' and anulado = 0 ");
-												$porcentaje    = $creditos_tmp->porcentaje/100;
-											}
-									
-											/*si es notas debito*/
-											
-											if($dcxc->tipo_documento_id == $notastipo){
-												$notasd_tmp = $this->NotasDebito->findFirst("id = '$encabezado->creditos_id' and anulado = 0 ");
-												$porcentaje = $notasd_tmp->porcentaje/100;
-											}
-									
-										
-											$capitaltmp = $items->valor / (1 + $porcentaje );
-											
-									}
+									$capitaltmp = $items->valor / (1 + $porcentaje );
 									
 									
 									$detalles->detalle_cxc_id         = $items->detalle_cxc_id;
