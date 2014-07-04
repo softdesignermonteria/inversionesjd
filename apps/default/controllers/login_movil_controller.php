1bd5541e21c1ea4eaec3f66bbaf93d6fa4a0d03a
softdesignermonteria <administrador@softdesignermonteria.net>
2014-07-04 11:17:03 -0500

    login movil

--- apps/default/controllers/login_movil_controller.php
+++ apps/default/controllers/login_movil_controller.php
@@ -11,6 +11,60 @@
 		
 		}
 		
+		
+		public function validarAction(){
+			
+			$this->setResponse('view');
+			
+				$respuesta[0]=array("mensaje"=>"true","descripcion"=>"por defecto");	
+				$admin=$_REQUEST["login"];
+
+				$Usuario = new Admin();
+				$cobrador = new Cobradores();
+				$usuario = $Usuario->findFirst("usuario = '".$admin->usuario."'	and password = '".$admin->clave."'");
+				
+				
+				if($usuario){
+					 $cobrador = $cobrador->findFirst(" id = '"+$admin->cobradores_id+"' ");
+					 $respuesta[0]=array("mensaje"=>"true","descripcion"=>"Usuario Registrado");	
+					 $respuesta[1]=array(
+					 						"nombre"=>$admin->nombre_completo,
+											"clave"=>$admin->password,
+											"cobradores_id"=>$admin->cobradores_id,
+											"cedula_cobrador"=>$cobrador->nit
+										);	
+					 //nombre,clave,cobradores_id,cedula_cobrador
+					 $syslogger = new Syslogger();
+					 $syslogger->username      = '';
+					 $syslogger->module        = Router::getModule();
+					 $syslogger->application   = Router::getApplication();
+					 $syslogger->controller    = $this->getControllerName();
+					 $syslogger->action        = $this->getActionName();
+					 $syslogger->error_sistema = "Usuario Registrado = '".$admin->usuario."' url =  '". $this->getControllerName()."/".$this->getActionName()."'  Fecha: ".date("Y-m-d H:i:s");
+					 $syslogger->descripcion   = "Usuario Registrado";
+					 $syslogger->ip_remota     = $_SERVER['REMOTE_ADDR'];
+					 $syslogger->fecha         = date("Y-m-d H:i:s");
+					 $syslogger->save();
+						
+				} else {
+					 $respuesta[0]=array("mensaje"=>"false","descripcion"=>"Usuario Clave incorrectos");	
+					 $syslogger = new Syslogger();
+					 $syslogger->username      = '';
+					 $syslogger->module        = Router::getModule();
+					 $syslogger->application   = Router::getApplication();
+					 $syslogger->controller    = $this->getControllerName();
+					 $syslogger->action        = $this->getActionName();
+					 $syslogger->error_sistema = "Usuario Clave incorrectos "."usuario = '".$admin->usuario."'	and password = '".$admin->clave."'  url= ". $this->getControllerName()."/".$this->getActionName()."'  Fecha: ".date("Y-m-d H:i:s");
+					 $syslogger->descripcion   = "Usuario Clave incorrectos";
+					 $syslogger->ip_remota     = $_SERVER['REMOTE_ADDR'];
+					 $syslogger->fecha         = date("Y-m-d H:i:s");
+					 $syslogger->save();
+
+				}
+
+		
+		}
+		
 	
 		
 }
