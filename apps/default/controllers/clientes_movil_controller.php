4ad1c369fcc2ad1d86c09eda6b630b3e652d6396
Alejandro Betanciurt <administrador@softdesignermonteria.net>
2014-07-06 11:49:40 -0500

    modificacion de clientes

--- apps/default/controllers/clientes_movil_controller.php
+++ apps/default/controllers/clientes_movil_controller.php
@@ -54,7 +54,7 @@
 										$cli->nombre2        = trim($clientes->nombre2);
 										$cli->apellido1      = trim($clientes->apellido1);
 										$cli->apellido2      = trim($clientes->apellido2);
-										$cli->razon_social   = trim($clientes->nombre1 + " " + $clientes->nombre2 + " " + $clientes->apellido1 + " " + $clientes->apellido2);
+										$cli->razon_social   = $clientes->nombre1 + " " + $clientes->nombre2 + " " + $clientes->apellido1 + " " + $clientes->apellido2;
 										$cli->direccion      = $clientes->direccion;
 										$cli->celular        = $clientes->telefono;
 										$cli->departamentos_id     = 23;
